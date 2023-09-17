1175779e022526e245084b2d94d24688d0af5cb1
jhuntbach-bc <jhuntbach@blockchain.com>
2023-09-17 18:25:43 +0100

    Remove variable scale factor

--- src/main/kotlin/com/jordan/pacman/Globals.kt
+++ src/main/kotlin/com/jordan/pacman/Globals.kt
@@ -4,5 +4,5 @@ object Globals {
     const val PIXELS_PER_TILE = 8
     const val SCALING_FACTOR = 3
     const val TILE_SIZE = PIXELS_PER_TILE * SCALING_FACTOR
-    const val SPRITES_BASE_PATH = "sprites/${TILE_SIZE}x${TILE_SIZE}"
+    const val SPRITES_BASE_PATH = "sprites"
 }
