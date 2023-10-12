f4bc26f190c55dd0ed021bdc1e6f030ae504b0bc
jhuntbach-bc <jhuntbach@blockchain.com>
2023-10-12 16:36:20 +0100

    slow down MCTS agent so we can watch it play

--- src/main/kotlin/com/jordan/pacman/ai/mcts/MCTSAgent.kt
+++ src/main/kotlin/com/jordan/pacman/ai/mcts/MCTSAgent.kt
@@ -5,6 +5,7 @@ import com.jordan.pacman.ai.mcts.expansion.JunctionBasedExpansionPolicy
 import com.jordan.pacman.ai.mcts.selection.UCTSelectionPolicy
 import com.jordan.pacman.ai.mcts.simulation.MomentumSimulationPolicy
 import com.jordan.pacman.game.Game
+import com.jordan.pacman.targeting
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import mu.KLogging
@@ -27,8 +28,10 @@ class MCTSAgent(
                 val mcts = MCTS(game, selectionPolicy, expansionPolicy, simulationPolicy, evaluationPolicy)
                 mcts.run(60)
                 mcts.selectBestMoves()?.forEach { direction ->
-                    game.playSingleFrame(direction)
-                    game.render()
+                    targeting(millisPerFrame = 12) {
+                        game.playSingleFrame(direction)
+                        game.render()
+                    }
                 }
             }
 
